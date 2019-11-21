db8733afd9485490d367c4adbb26c7f45751da14
jouanneaur <55240056+jouanneaur@users.noreply.github.com>
2019-11-21 17:39:20 +0100

    graph selon access

--- partials/graph.php
+++ partials/graph.php
@@ -1,7 +1,7 @@
 <?php  
 
 
-$sql = "SELECT data_f.data FROM logem INNER JOIN data_f ON logem.id_f = data_f.id_f WHERE logem.id_f = 1 ";
+$sql = "SELECT data_f.data FROM access INNER JOIN data_f ON access.id_u = data_f.id_f WHERE access.id_u = $row2[0]";
 $result = $db->query($sql);
 
 if ($result->num_rows > 0) {
